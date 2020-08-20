95b76ffcc44573cef1e499d4c7103c078eb4f826
remi <remi.frazier@ucsf.edu>
2020-08-19 23:40:42 -0700

    $this

--- MultiSignatureConsent.php
+++ MultiSignatureConsent.php
@@ -73,7 +73,7 @@ class MultiSignatureConsent extends \ExternalModules\AbstractExternalModule {
     $logics = $this->framework->getSubSettings('compilation-passes');
 
     foreach ($logics as $n=>$logic) {
-        $evalLogicArr[$n]           = $logic['eval-logic'];
+        $this->$evalLogicArr[$n]           = $logic['eval-logic'];
         $destinationFileFieldArr[$n]           = $logic['destination-file-field'];
         $headerArr[$n]           = $logic['header'];
         $footerArr[$n]           = $logic['footer'];
